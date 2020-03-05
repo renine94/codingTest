c8925f9cdf727e0a595d9a258673b6892b82c9ef
Jaegu Kang <kjaegu9771@naver.com>
2020-03-05 14:36:17 +0900

    2020/03/05 [modify] 2819 재귀풀이 추가

--- swea/D4/2819_격자판.py
+++ swea/D4/2819_격자판.py
@@ -22,3 +22,26 @@ for tc in range(1, T+1):
                         stack.append((nx, ny, z+matrix[nx][ny]))
             
     print('#{} {}'.format(tc, len(result)))
+
+# 재귀버전으로 풀기
+
+# def find(i, j, n, s):
+#     di = [0, 1, 0, -1]
+#     dj = [1, 0, -1, 0]
+#     if (n==7):
+#         t.add(s)
+#     else:
+#         for k in range(4):
+#             ni = i + di[k]
+#             nj = j + dj[k]
+#             if 0 <= ni < 4 and 0 <= nj < 4:
+#                 find(ni, nj, n+1, s + str(a[i][j]))
+
+# T = int(input())
+# for tc in range(1, T+1):
+#     a = [list(map(int, input().split())) for _ in range(4)]
+#     t = set()
+#     for i in range(4):
+#         for j in range(4):
+#             find(i, j, 0, '')
+#     print('#{} {}'.format(tc, len(t)))

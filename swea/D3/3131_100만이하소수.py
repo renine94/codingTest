67187db63d97f256b188557fc971be330083cec0
Jaegu Kang <kjaegu9771@naver.com>
2020-03-08 20:11:50 +0900

    2020/03/08 [add] 3131

---
+++ swea/D3/3131_100만이하소수.py
@@ -0,0 +1,12 @@
+# 에라토스테네스의 체
+# https://wikidocs.net/21638
+n=pow(10, 6)
+a = [False,False] + [True]*(n-1)
+primes=[]
+
+for i in range(2,n+1):
+  if a[i]:
+    primes.append(i)
+    for j in range(2*i, n+1, i):
+        a[j] = False
+print(*primes)

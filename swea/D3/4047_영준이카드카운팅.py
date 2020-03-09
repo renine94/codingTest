90f2d2050db5ca0b4d9e7a2477617c73b08849e2
강재구 <57125936+94incheon@users.noreply.github.com>
2020-03-09 17:52:29 +0900

    Update 4047_영준이카드카운팅.py
    숏코딩 다른사람 풀이 맨아래 추가

--- swea/D3/4047_영준이카드카운팅.py
+++ swea/D3/4047_영준이카드카운팅.py
@@ -38,3 +38,12 @@ for tc in range(1, int(input()) + 1):
                 C_cnt -= 1
         
         print('#{} {} {} {} {}'.format(tc, S_cnt, D_cnt, H_cnt, C_cnt))
+        
+        
+        
+# for t in range(1,int(input())+1):
+#     s=input();c={'S':13,'D':13,'H':13,'C':13}
+#     for i in range(0,len(s),3):
+#         c[s[i]]-=1
+#         if s.count(s[i:i+3])>1:c={0:'ERROR'};break
+#     print('#%i'%t,*c.values())

8c77edae943468770caf6e469918e12ee0204b58
Jaegu Kang <kjaegu9771@naver.com>
2020-03-12 00:45:55 +0900

    2020/03/12 [add] 1230

---
+++ swea/D3/1230_암호문3.py
@@ -0,0 +1,36 @@
+import sys
+sys.stdin = open('input.txt')
+
+T=10
+for tc in range(1, T+1):
+    N = int(input()) # 원본 암호문의 길이
+    password = input().split()
+    M = int(input()) # 명령어의 개수
+    command = input().split()
+
+    for idx, cmd in enumerate(command):
+        if cmd == 'I':
+            p = int(command[idx+1])
+            i = int(command[idx+2])
+
+            lst = password[:p]
+            lst.extend(command[idx+3 : idx+3+i])
+            lst.extend(password[p:])
+
+            password = lst
+
+        elif cmd == 'D':
+            x = int(command[idx+1])
+            y = int(command[idx+2])
+
+            for _ in range(y):
+                password.pop(x)
+        
+        elif cmd == 'A':
+            y = int(command[idx+1])
+            s = list(map(int, command[idx+2 : idx+2+y]))
+            password.extend(s)
+
+    result = password[:10]
+
+    print('#{}'.format(tc), *result)

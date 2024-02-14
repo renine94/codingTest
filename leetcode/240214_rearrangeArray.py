814405684f38b4be58f1b89ac336d86587ea872b
JaeguKang <renine94.dev@gmail.com>
2024-02-14 10:51:21 +0900

    leetcode: 정수를 조건대로 재정렬하여 결과 만들기

---
+++ leetcode/240214_rearrangeArray.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/rearrange-array-elements-by-sign/
+
+from typing import List
+
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        positive, negative = [], []
+        for num in nums:
+            if num > 0:
+                positive.append(num)
+            else:
+                negative.append(num)
+        return [item for sublist in list(zip(positive, negative)) for item in sublist]
+
+
+
+nums, expected = [3, 1, -2, -5, 2, -4], [3, -2, 1, -5, 2, -4]
+result = Solution().rearrangeArray(nums)
+
+assert result == expected

1a56803f5776064880440a46e13c6dd85e7bba7e
JaeguKang <renine94.dev@gmail.com>
2024-02-14 10:53:11 +0900

    leetcode: 정수를 조건대로 재정렬하여 결과 만들기 - 리팩토링

--- leetcode/240214_rearrangeArray.py
+++ leetcode/240214_rearrangeArray.py
@@ -11,6 +11,13 @@ class Solution:
             else:
                 negative.append(num)
         return [item for sublist in list(zip(positive, negative)) for item in sublist]
+    
+
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        positive = [num for num in nums if num > 0] 
+        negative = [num for num in nums if num < 0] 
+        return [item for sublist in list(zip(positive, negative)) for item in sublist]
 
 
 

3d75c4c6c4d3ee28548afc49d3254ac7230a4b0e
jaegu kang <renine94.dev@gmail.com>
2024-05-16 13:15:41 +0900

    leetcode: 노드 재귀 순환 #2 주석추가

--- leetcode/2405/240516_evaluateTree.py
+++ leetcode/2405/240516_evaluateTree.py
@@ -8,9 +8,13 @@ class TreeNode:
         self.left = left
         self.right = right
 
+    def __repr__(self) -> str:
+        return f"<TreeNode val={self.val}, left={self.left}, right={self.right}>"
+
 
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        # 리프노드는 0 or 1 의 값만 가진다.
         if not root.left and not root.right:
             return root.val != 0
 
@@ -23,3 +27,8 @@ class Solution:
             evaluate_root = left and right
 
         return evaluate_root
+
+
+root = TreeNode(val=2, left=TreeNode(val=1), right=TreeNode(val=3, left=TreeNode(val=0), right=TreeNode(val=1)))
+result = Solution().evaluateTree(root)
+assert result == True
